ead5227365dc023627fe7786160e7faef21dd0b3
Prince Kr. Mishra <95128645+Prince04Mishra@users.noreply.github.com>
2025-01-03 13:14:42 +0530

    Update LoginPage.js

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -20,7 +20,7 @@ const LoginPage = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://e-com-uam9.vercel.app/api/auth/login",
         formData
       );
 
